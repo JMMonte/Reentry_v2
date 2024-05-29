340569c40af0a47647f84398e4d6d1c448cfb5e0
João Montenegro <joaommontenegro1@gmail.com>
2024-05-29 05:03:57 +0100

    Compressed textures

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ import { BackgroundStars } from './components/background.js';
 // import textures
 import earthTexture from '../public/assets/texture/8k_earth_daymap.jpg';
 import earthSpecTexture from '../public/assets/texture/8k_earth_specular_map.png';
-import earthNormalTexture from '../public/assets/texture/8k_earth_normal_map.png';
+import earthNormalTexture from '../public/assets/texture/8k_earth_normal_map.jpg';
 import cloudTexture from '../public/assets/texture/cloud_combined_8192.png';
 import moonTexture from '../public/assets/texture/lroc_color_poles_8k.jpg';
 import moonBump from '../public/assets/texture/ldem_16_uint.jpg';
